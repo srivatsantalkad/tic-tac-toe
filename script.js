079296a94521a379bcc909d5ca4ae65f7e11f59f
Srivatsan Talkad <stalkad01@gmail.com>
2022-01-25 22:51:34 -0500

    Edit index.html, script.js, style.css

---
+++ script.js
@@ -0,0 +1,21 @@
+const Gameboard = (() => {
+    let gameboardArray = [];
+
+    const addMove = (move) => {
+        gameboardArray.push(move);
+    };
+
+})();
+
+const Player = ((x, y) => {
+    let x = x;
+    let y = y;
+
+})();
+
+const Game = (() => {
+
+})();
+
+
+
